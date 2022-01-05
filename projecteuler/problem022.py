762b6cd20671b3ee642862d398adc38b18e3b456
masterlamp <master@la.mps>
2022-01-05 21:10:33 +0100

    Add comment to problem 22

--- projecteuler/problem022.py
+++ projecteuler/problem022.py
@@ -5,7 +5,9 @@ with open('projecteuler/problem022_names.txt', "r") as file:
     sumName = 0
     nameIndex = 1
     for n in data:
+        #clear quotes
         n = n.replace("\"","")
+        #convert to ascii and remove offset(64)
         nameSum = sum((ord(n[l])-64) for l in range(len(n)))
         sumName += (nameSum*nameIndex)
         nameIndex += 1
