69230ccb060324ec80b7c244909777393faaee37
masterlamp <master@la.mps>
2022-01-05 18:02:57 +0100

    Solve problem 14

--- projecteuler/problem014.py
+++ projecteuler/problem014.py
@@ -1,23 +1,19 @@
-s = 0
 savedS = 0
-result = 0
+savedN = 0
 
 for n in reversed(range(1000000)):
-    s = 1
+    s = 0
     curN = n
     for i in reversed(range(n)):
-        #print(n)
         if((n % 2) == 0):
             n = n / 2
         else:
             n = 3 * n + 1
         s += 1
         if(n == 1):
-            #print(n)
             break
     if(savedS < s):
-        result = s
         savedS = s
-        print(str(curN) + " " + str(result))
+        savedN = curN
 
-print(result)
+print(str(savedN) + " " + str(savedS))
